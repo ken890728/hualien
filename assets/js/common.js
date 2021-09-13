a8d77fdf83141613d31236bb9edc6a4b231a318c
Ken <s1110434045@gms.nutc.edu.tw>
2021-09-13 21:39:19 +0800

    RWD header and pages update

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,3 +2,15 @@ $(function() {
     $("#header").load("./header.html");
     $("#footer").load("./footer.html");
 });
+
+var hide = true;
+
+function open_phone_list() {
+    if (hide) {
+        $(".phone-list").show(300);
+        hide = false;
+    } else {
+        $(".phone-list").hide(300);
+        hide = true;
+    }
+}
