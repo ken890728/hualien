00fc331c26927ea3e4fb39024a6c77080cd82c8e
Ken <s1110434045@gms.nutc.edu.tw>
2021-09-22 18:48:34 +0800

    new board test update

---
+++ assets/js/common_test.js
@@ -0,0 +1,22 @@
+$(function() {
+    $("#header").load("./header_test.html");
+    $("#footer").load("./footer_test.html");
+});
+
+window.onload = function() {
+    var content_height = $(document.body).height() - $("header").height() - $("footer").height();
+    $("article").css("min-height", content_height + "px");
+}
+
+
+var hide = true;
+
+function open_phone_list() {
+    if (hide) {
+        $(".phone-list").show(300);
+        hide = false;
+    } else {
+        $(".phone-list").hide(300);
+        hide = true;
+    }
+}
