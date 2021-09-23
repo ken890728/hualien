ff0975edf2898bb261d13c17bb61cfeb7af563db
Ken <s1110434045@gms.nutc.edu.tw>
2021-09-23 12:16:39 +0800

    header footer update

--- assets/js/member.js
+++ assets/js/member.js
@@ -2,20 +2,3 @@ $(function() {
     $("#header").load("./header_member.html");
     $("#footer").load("./footer.html");
 });
-
-window.onload = function() {
-    var content_height = $(document.body).height() - $("header").height() - $("footer").height();
-    $("article").css("min-height", content_height + "px");
-}
-
-var hide = true;
-
-function open_phone_list() {
-    if (hide) {
-        $(".phone-list").show(300);
-        hide = false;
-    } else {
-        $(".phone-list").hide(300);
-        hide = true;
-    }
-}
