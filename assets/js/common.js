b70a612dd1ec4edd619b069032a937058d577665
Ken <s1110434045@gms.nutc.edu.tw>
2021-09-27 16:57:27 +0800

    update search bar

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,3 +2,7 @@ $(function() {
     $("#header").load("./header.html");
     $("#footer").load("./footer.html");
 });
+
+function show_search() {
+    $('#search-input-block').show(200);
+}
--- assets/js/member.js
+++ assets/js/member.js
@@ -2,3 +2,7 @@ $(function() {
     $("#header").load("./header_member.html");
     $("#footer").load("./footer.html");
 });
+
+function show_search() {
+    $('#search-input-block').show(200);
+}
